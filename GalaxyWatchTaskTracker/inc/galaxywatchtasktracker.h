2eb5396b0b88df94d628a4e860cd1be9c5461768
alanger <mightydodongo@gmail.com>
2021-04-24 13:00:59 -0500

    Initial Project Setup

---
+++ GalaxyWatchTaskTracker/inc/galaxywatchtasktracker.h
@@ -0,0 +1,19 @@
+#ifndef __galaxywatchtasktracker_H__
+#define __galaxywatchtasktracker_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "galaxywatchtasktracker"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.galaxywatchtasktracker"
+#endif
+
+#endif /* __galaxywatchtasktracker_H__ */
